acddd22128562798aa95884cf651ec367f53e80c
Sandy Obare <obaresandy@gmail.com>
2023-11-03 15:07:52 +0000

    addition of main.h header files containing libraries expected to be used

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+extern char **environ;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <dirent.h>
+#include <signal.h>
+#include <errno.h>
+
+#endif /* MAIN_H */
